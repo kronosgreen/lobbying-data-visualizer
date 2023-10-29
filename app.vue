7d0463d9913b0fdb7ab40c65801b0ea87b659362
Christopher Medrano <703137326@uniparks.com>
2023-10-28 19:06:15 -0600

    set up apollo client pointing to local server

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>There are {{ data?.firmsAggregate?.count || 0 }} firms.</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+const query = gql`
+query numFirms {
+    firmsAggregate {
+        count
+    }
+}`
+
+// const variables = { limit: 5 }
+const variables = {}
+
+const { data } = await useAsyncQuery(query, variables)
+
+</script>
