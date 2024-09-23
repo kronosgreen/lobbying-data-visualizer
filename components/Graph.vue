eca7a5a26206272a2b2ddf7816f8a4d66153773c
Derick Vigne <derickvigne@me.com>
2024-09-23 14:06:40 -0400

    Added Loading States and Enabled Client Side Rendering of Sigma Graph

--- components/Graph.vue
+++ components/Graph.vue
@@ -1,29 +1,30 @@
 <template>
-    <Suspense>
-        <ClientOnly>
-            <div class="max-w-screen-xl grid grid-cols-6 gap-4 h-1/5 mx-5 px-5 my-5 py-2" >
-                <div class="col-span-2 relative mb-6">
-                    <label for="default-range" class="block mb-2 text-sm font-medium text-gray-900">Minimum Total Spent</label>
-                    <input id="default-range" type="range" min="0" max="30000000" step="10000" value="10000000" class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700">
-                    <span class="text-sm text-gray-500 dark:text-gray-400 absolute start-0 -bottom-6">Min ($0)</span>
-                    <span class="text-sm text-gray-500 dark:text-gray-400 absolute end-0 -bottom-6">Max ($30000000)</span>
-                </div>
+  <div id="graph-render">
+    <div class="max-w-screen-xl grid grid-cols-6 gap-4 h-1/5 mx-5 px-5 my-5 py-2" >
+        <div class="col-span-2 relative mb-6">
+            <label for="default-range" class="block mb-2 text-sm font-medium text-gray-900">Minimum Total Spent</label>
+            <input id="default-range" type="range" min="0" max="30000000" step="10000" value="10000000" class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700">
+            <span class="text-sm text-gray-500 dark:text-gray-400 absolute start-0 -bottom-6">Min ($0)</span>
+            <span class="text-sm text-gray-500 dark:text-gray-400 absolute end-0 -bottom-6">Max ($30000000)</span>
+        </div>
 
-                <div>
+        <div>
 
-                </div>
+        </div>
 
-                <button class="row-auto m-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                    Refresh Query
-                </button>
-            </div>
-            <Sigma :graph="graph" :sectorColors="sector_color" fallback="Loading..."/>
-        </ClientOnly>
-    </Suspense>
+        <button class="row-auto m-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+            Refresh Query
+        </button>
+    </div>
+    <span v-if="loading" class="loader"></span>
+    <Sigma v-else :graph="graph" :sectorColors="sector_color" fallback="Loading..."/>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import Graph from "graphology"
+
+const loading = ref(true);
 
 const query = gql`
 query TopSpendingFirms($fw: FirmWhere) {
@@ -66,7 +67,7 @@ const variables = {
 const graph = new Graph();
 let sector_color: any = {};
 
-await useAsyncQuery(query, variables).then((results: any) => {
+useAsyncQuery(query, variables).then((results: any) => {
     let palette: string[] = [
         "#fd7f6f",
         "#7eb0d5",
@@ -138,8 +139,44 @@ await useAsyncQuery(query, variables).then((results: any) => {
         }
     })
     console.log(empDropped + " employees dropped")
-
-})
+    loading.value = false;
+});
 
  </script>
  
+ <style>
+.loader {
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  position: absolute;
+  left: 48vw;
+  top: 48vh;
+  animation: rotate 1s linear infinite
+}
+.loader::before , .loader::after {
+  content: "";
+  box-sizing: border-box;
+  position: absolute;
+  inset: 0px;
+  border-radius: 50%;
+  border: 5px solid #000;
+  animation: prixClipFix 2s linear infinite ;
+}
+.loader::after{
+  transform: rotate3d(90, 90, 0, 180deg );
+  border-color: #3b82f6;
+}
+
+@keyframes rotate {
+  0%   {transform: rotate(0deg)}
+  100%   {transform: rotate(360deg)}
+}
+
+@keyframes prixClipFix {
+    0%   {clip-path:polygon(50% 50%,0 0,0 0,0 0,0 0,0 0)}
+    50%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 0,100% 0,100% 0)}
+    75%, 100%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 100%,100% 100%,100% 100%)}
+}
+
+</style>
